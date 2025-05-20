e54d216de85228e10dfd4368746bf51eaa250231
CHUSUEYEON <rmffkfk226@naver.com>
2025-05-21 00:12:07 +0900

    ✨feat-main: 메인 페이지 대략적인 구조 작업

---
+++ frontend/src/components/ranking/RankingList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="ranking-list">
+    <RankingItem />
+  </div>
+</template>
+
+<script setup>
+import RankingItem from "./RankingItem.vue";
+</script>
+
+<style scoped></style>
